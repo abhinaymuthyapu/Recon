cc9b93b0c260d2c8ce95d4d34b14967c909f50e3
abhinaymuthyapu <abhinay.muthyapu@gmail.com>
2019-06-05 22:19:03 +0530

    First Recon Code Commit

---
+++ Recon/src/main/java/Recon/Recon/App.java
@@ -0,0 +1,13 @@
+package Recon.Recon;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
