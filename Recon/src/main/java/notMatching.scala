4c4b88477c4aacd11b7b3fc54fefa84812e36694
718728 <718728@infpw11209.pwiodc.lntinfotech.com>
2019-06-08 01:26:28 +0530

    Code 6/8 01:26AM

--- Recon/src/main/java/notMatching.scala
+++ Recon/src/main/java/notMatching.scala
@@ -3,20 +3,21 @@ import org.apache.spark.SparkContext._
 import org.apache.spark.SparkConf
 import org.apache.log4j._
 
-object Recon {
+object notMatching {
   def main (args: Array[String]){
    System.setProperty("hadoop.home.dir", "C:\\winutils")
    Logger.getLogger("org").setLevel(Level.ERROR)
-    val sc = new SparkContext("local[*]", "ReconRR")
+    val sc = new SparkContext("local[*]", "notMatching")
     val sqlContext= new org.apache.spark.sql.SQLContext(sc)
-    val df = sqlContext.read.json("src/main/resources/people.json")
+    val df = sqlContext.read.json("src/main/resources/Test1.json")
     df.createTempView("table")
     val df3 = sqlContext.sql("SELECT name FROM table")
-   // df3.show()
-    val path = "src/main/resources/people.csv"
+    val path = "src/main/resources/Test1.csv"
     val base_df = sqlContext.read.option("header","true").csv(path)
     base_df.createTempView("table1")
-    val base_df3 = sqlContext.sql("select * from table1 intersect select * from table")
-    base_df3.show()
+    val A_B = sqlContext.sql("select * from table1 minus select * from table").createTempView("A_B")
+    val B_A = sqlContext.sql("select * from table minus select * from table1").createTempView("B_A")
+    val notMathing = sqlContext.sql("select * from A_B full_outer select * from B_A")
+    notMathing.show()
 }
 }
--- Recon/src/main/java/rightOuter.scala
+++ Recon/src/main/java/rightOuter.scala
@@ -3,20 +3,19 @@ import org.apache.spark.SparkContext._
 import org.apache.spark.SparkConf
 import org.apache.log4j._
 
-object Recon {
+object rightOuter {
   def main (args: Array[String]){
-   System.setProperty("hadoop.home.dir", "C:\\winutils")
-   Logger.getLogger("org").setLevel(Level.ERROR)
+     System.setProperty("hadoop.home.dir", "C:\\winutils")
+  Logger.getLogger("org").setLevel(Level.ERROR)
     val sc = new SparkContext("local[*]", "ReconRR")
     val sqlContext= new org.apache.spark.sql.SQLContext(sc)
     val df = sqlContext.read.json("src/main/resources/people.json")
     df.createTempView("table")
     val df3 = sqlContext.sql("SELECT name FROM table")
-   // df3.show()
     val path = "src/main/resources/people.csv"
     val base_df = sqlContext.read.option("header","true").csv(path)
     base_df.createTempView("table1")
-    val base_df3 = sqlContext.sql("select * from table1 intersect select * from table")
-    base_df3.show()
-}
+    val outer_join = df.join(base_df, df("name") === base_df("name"), "right_outer")
+    outer_join.show()
+   }
 }
