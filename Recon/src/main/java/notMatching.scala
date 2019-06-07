358d5eb09b04ee942d494773f476946170a77bc8
718728 <718728@infpw11209.pwiodc.lntinfotech.com>
2019-06-08 01:41:18 +0530

    notMatching

--- Recon/src/main/java/notMatching.scala
+++ Recon/src/main/java/notMatching.scala
@@ -11,13 +11,15 @@ object notMatching {
     val sqlContext= new org.apache.spark.sql.SQLContext(sc)
     val df = sqlContext.read.json("src/main/resources/Test1.json")
     df.createTempView("table")
+    df.show()
     val df3 = sqlContext.sql("SELECT name FROM table")
     val path = "src/main/resources/Test1.csv"
     val base_df = sqlContext.read.option("header","true").csv(path)
+    base_df.show()
     base_df.createTempView("table1")
     val A_B = sqlContext.sql("select * from table1 minus select * from table").createTempView("A_B")
     val B_A = sqlContext.sql("select * from table minus select * from table1").createTempView("B_A")
     val notMathing = sqlContext.sql("select * from A_B full_outer select * from B_A")
-    notMathing.show()
+   // notMathing.show()
 }
 }
