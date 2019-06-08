9234160e7aa8fb93425ec56ca3d579ea5a2d0341
10649886 <10649886@INFPW07356.pwiodc.lntinfotech.com>
2019-06-08 06:47:20 +0530

    main class

--- HRecon/src/com/Recon.scala
+++ HRecon/src/com/Recon.scala
@@ -3,6 +3,7 @@ import org.apache.spark._
 import org.apache.spark.SparkContext._
 import org.apache.spark.SparkConf
 import org.apache.log4j._
+import com.recon.config.ConfigReader
 object Recon {
   def main (args: Array[String]){
    System.setProperty("hadoop.home.dir", "C:\\winutils")
@@ -12,11 +13,14 @@ object Recon {
     val df = sqlContext.read.json("src/resources/people.json")
     df.createTempView("table")
     val df3 = sqlContext.sql("SELECT name FROM table")
-    df3.show()
+   // df3.show()
     val path = "src/resources/people.csv"
     val base_df = sqlContext.read.option("header","true").csv(path)
     base_df.createTempView("table1")
-    val base_df3 = sqlContext.sql("select * from table1 intersect select * from table")
-    base_df3.show()
+    //val base_df3 = sqlContext.sql("select * from table1 intersect select * from table")
+    //base_df3.show()
+    val cr = new ConfigReader()
+   val _type = cr.getConfig(sc,"config.source.url");
+   println(_type)
 }
 }
