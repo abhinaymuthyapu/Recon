17970dd4734f01a5eb1fbbf75de23068cd0ac2ea
10649886 <10649886@INFPW07356.pwiodc.lntinfotech.com>
2019-06-08 06:40:42 +0530

    ConfigReader

--- HRecon/src/com/recon/config/ConfigReader.scala
+++ HRecon/src/com/recon/config/ConfigReader.scala
@@ -8,6 +8,9 @@ import org.apache.log4j._
 
 class ConfigReader {
   
-  //val myDF = spark.read.json("/Config")
-  //val config = myDF.select("config")
+    def getConfig(sc: SparkContext, path: String): Any = {  
+    val sqlContext= new org.apache.spark.sql.SQLContext(sc)
+    val df1 = sqlContext.read.json(sc.wholeTextFiles("src/resources/config.json").values)
+    return df1.select(path).first().get(0)
+    }
 }
